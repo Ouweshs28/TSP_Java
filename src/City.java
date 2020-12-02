e44e3cc72af84d67cabe59b05e3472fcb91d6aa2
Ouweshs28 <ouweshseeroo@gmail.com>
2020-12-02 18:26:52 +0400

    added get distance method

--- src/City.java
+++ src/City.java
@@ -1,5 +1,30 @@
 public class City {
     private int postion;
+
+    public int getPostion() {
+        return postion;
+    }
+
+    public void setPostion(int postion) {
+        this.postion = postion;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
     private int xCoordinate;
     private int yCoordinate;
 
@@ -15,6 +40,17 @@ public class City {
         xCoordinate=x;
         yCoordinate=y;
     }
+
+    // Gets the distance to given city
+    public double distanceTo(City city){
+        int xDistance = Math.abs(getxCoordinate() - city.getxCoordinate());
+        int yDistance = Math.abs(getyCoordinate() - city.getyCoordinate());
+        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+
+        return distance;
+    }
+
+
 
     void printCity(){
         System.out.println("Position: "+postion+" ,X-Coordinate: "+xCoordinate+ " ,Y-Coordinate: "+yCoordinate);
