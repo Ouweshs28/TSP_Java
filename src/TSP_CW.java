eda63b3e7cd8f8ce968340a66eaa575980f5abd0
Ouweshs28 <ouweshseeroo@gmail.com>
2020-12-03 00:42:43 +0400

    clean base\

--- src/TSP_CW.java
+++ src/TSP_CW.java
@@ -4,24 +4,6 @@ public class TSP_CW {
     public static void main(String[] args) {
         FileManagement cityFile=new FileManagement();
         ArrayList <City> cities=cityFile.readFile("test3atsp.txt");
-
-        for(City c:cities){
-            TourManager.addCity(c);
-        }
-        Population pop = new Population(50, true);
-        System.out.println("Initial distance: " + pop.getFittest().getDistance());
-
-        // Evolve population for 100 generations
-        pop = GA.evolvePopulation(pop);
-        for (int i = 0; i < 100; i++) {
-            pop = GA.evolvePopulation(pop);
-        }
-
-        // Print final results
-        System.out.println("Finished");
-        System.out.println("Final distance: " + pop.getFittest().getDistance());
-        System.out.println("Solution:");
-        System.out.println(pop.getFittest());
     }
 
 }
